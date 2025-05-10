f52197f10f5313f27ca8ca15d81c63ae35ef5229
MTM3MDM <choipg326868@gmail.com>
2025-05-10 16:26:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import os  # ✅ 이 줄 추가!
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='쿠루미', intents=intents)
@@ -10,6 +11,6 @@ async def on_ready():
 
 @bot.command()
 async def 핑(ctx):
-    await ctx.send('퐁!')
+    await ctx.send("퐁!")
 
 bot.run(os.getenv("TOKEN"))
